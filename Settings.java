1409e7bb728771542cb97cd9396f65cd4e2e16f0
A7medDzMods <fhfhfyfyfyfffh@gmail.com>
2026-03-18 23:23:52 +0100

    Add Settings class for killaura configuration

---
+++ Settings.java
@@ -0,0 +1,15 @@
+package com.hypixelmod.killaura;
+
+import org.lwjgl.input.Keyboard;
+
+public class Settings {
+    public static boolean killauraEnabled = false;
+    public static boolean autoblock = false;
+    public static double reach = 3.8;
+    public static int cps = 12;
+    public static int toggleKey = Keyboard.KEY_R;
+
+    public static void toggle() {
+        killauraEnabled = !killauraEnabled;
+    }
+  }
