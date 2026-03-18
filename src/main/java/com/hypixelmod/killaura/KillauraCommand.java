1dfa1570cd0542d5d6b47eac091f36ba802fc2b1
A7medDzMods <fhfhfyfyfyfffh@gmail.com>
2026-03-18 23:36:22 +0100

    Add KillauraCommand class for key input handling

---
+++ src/main/java/com/hypixelmod/killaura/KillauraCommand.java
@@ -0,0 +1,25 @@
+package com.hypixelmod.killaura;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import org.lwjgl.input.Keyboard;
+
+@Mod(modid = "killaura", name = "Killaura Mod", version = "1.0")
+public class KillauraMod {
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new KillauraCommand());
+    }
+
+    @SubscribeEvent
+    public void onKey(InputEvent.KeyInputEvent event) {
+        if (Keyboard.getEventKeyState() && Keyboard.getEventKey() == Settings.toggleKey) {
+            Settings.toggle();
+        }
+    }
+            }
