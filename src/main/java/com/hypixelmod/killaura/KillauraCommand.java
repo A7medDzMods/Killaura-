e5f8beffd9f8d429acf39d7714a0569c5eb63896
A7medDzMods <fhfhfyfyfyfffh@gmail.com>
2026-03-18 23:37:38 +0100

    Add chat command support for Killaura settings
    Implemented chat command handling for Killaura features.

--- src/main/java/com/hypixelmod/killaura/KillauraCommand.java
+++ src/main/java/com/hypixelmod/killaura/KillauraCommand.java
@@ -1,25 +1,44 @@
 package com.hypixelmod.killaura;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import org.lwjgl.input.Keyboard;
 
-@Mod(modid = "killaura", name = "Killaura Mod", version = "1.0")
-public class KillauraMod {
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.register(new KillauraCommand());
-    }
+public class KillauraCommand {
 
     @SubscribeEvent
-    public void onKey(InputEvent.KeyInputEvent event) {
-        if (Keyboard.getEventKeyState() && Keyboard.getEventKey() == Settings.toggleKey) {
-            Settings.toggle();
+    public void onChatMessage(ClientChatEvent event) {
+        String msg = event.message;
+
+        if (msg.startsWith(".killaura ")) {
+            event.setCanceled(true);
+            String[] args = msg.split(" ");
+            if (args.length < 2) return;
+
+            String sub = args[1].toLowerCase();
+            switch (sub) {
+                case "on": Settings.killauraEnabled = true; break;
+                case "off": Settings.killauraEnabled = false; break;
+                case "autoblock": 
+                    if (args.length > 2) Settings.autoblock = args[2].equalsIgnoreCase("on");
+                    break;
+                case "setcps":
+                    if (args.length > 2) Settings.cps = Integer.parseInt(args[2]);
+                    break;
+                case "setreach":
+                    if (args.length > 2) Settings.reach = Double.parseDouble(args[2]);
+                    break;
+                case "help":
+                    sendMsg(".killaura on/off, setreach, setcps, autoblock on/off");
+                    return;
+            }
+            sendMsg("Updated: " + sub);
         }
     }
-            }
+
+    private void sendMsg(String text) {
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "[Mod] " + text));
+    }
+                                                          }
